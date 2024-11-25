2051ded2d97955dbc27e77010e14a607f75a5c7b
ElCabal <cabalholguinlufe@gmail.com>
2024-11-25 03:16:23 -0500

    install adminlte template and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header/header.component';
+import { FooterComponent } from './layout/footer/footer/footer.component';
+import { SidebarComponent } from './layout/sidebar/sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
+      path: 'auth',
+      loadChildren: () => import('./features/auth/auth.routes')
+        .then(m => m.AUTH_ROUTES)
+    },
+];
