ce72dbfada9a4bbb88ef2e0d0bae91abf1cd4eab
ElCabal <cabalholguinlufe@gmail.com>
2024-12-02 20:24:06 -0500

    limpio codigo erroneo en header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { AlertService } from '../../../core/services/alert.service';
 import { AuthService } from '../../../core/services/auth.service';
-import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -12,24 +10,12 @@ import { ThemeService } from '../../../core/services/theme.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  private themeService = inject(ThemeService);
   private authService = inject(AuthService);
   private alertService = inject(AlertService);
-  isDarkMode = false;
-
-  constructor() {
-    this.themeService.darkMode$.subscribe(
-      isDark => this.isDarkMode = isDark
-    );
-  }
 
   get userFullName(): string {
     const user = this.authService.getCurrentUser();
     return user ? `${user.firstName} ${user.lastName}` : '';
-  }
-
-  toggleTheme() {
-    this.themeService.toggleDarkMode();
   }
 
   onLogout() {
